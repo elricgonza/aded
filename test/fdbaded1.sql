a9cbc104469b291d2561acb0dec3b02b427031e4
elricgonza <elricgonza@gmail.com>
2026-03-03 17:07:48 -0400

    fill e

--- test/fdbaded1.sql
+++ test/fdbaded1.sql
@@ -1,19 +1,22 @@
 
 ------
+delete from plan;
 insert into plan (id, plan) values (1, 'Bach Hum');
 insert into plan (id, plan) values (2, 'Bach Téc');
 insert into plan (id, plan) values (3, 'Tecn Med');
 
+/*
 delete from plan where id=3;  --x  ok
 update plan set id=9 where id=3  --x ok
 update plan set id=3 where id=9  --x ok
 select *
 	from plan a, nivel b
 	where a.id = b.id_plan
+*/	
 
 ------
 --insert into nivel (id, nivel, id_plan) values (1, 'Inic0', 0); --x ok
-
+delete from nivel;
 insert into nivel (id, nivel, id_plan) values (1, 'Inic', 1);
 insert into nivel (id, nivel, id_plan) values (2, 'Prim', 1);
 insert into nivel (id, nivel, id_plan) values (3, 'Sec', 1);
@@ -24,6 +27,7 @@ insert into nivel (id, nivel, id_plan) values (6, 'Interm', 3);
 
 ------
 --insert into grado  (id, grado, id_nivel) values (1, 'pre k', 0);  --x ok
+delete from grado;
 insert into grado  (id, grado, id_nivel) values (1, 'pre k', 1);
 insert into grado  (id, grado, id_nivel) values (2, 'kind', 1);
 
@@ -54,81 +58,81 @@ insert into grado  (id, grado, id_nivel) values (21, '3ro. Interm', 6);
 
 
 --------
---insert into materia(id, materia, id_grado) values (1, 'mat chit', 50);	--x ok
+--insert into mat(id, materia, id_grado) values (1, 'mat chit', 50);	--x ok
 --delete from materia
 
 --inic
-insert into materia(id, materia, id_grado) values (1, 'mat chit', 1);
-insert into materia(id, materia, id_grado) values (2, 'mat mov', 1);
+insert into mat(id, materia, id_grado) values (1, 'mat chit', 1);
+insert into mat(id, materia, id_grado) values (2, 'mat mov', 1);
 
-insert into materia(id, materia, id_grado) values (3, 'mat movis', 2);
-insert into materia(id, materia, id_grado) values (4, 'mat capor', 2);
-insert into materia(id, materia, id_grado) values (5, 'mat count', 2);
+insert into mat(id, materia, id_grado) values (3, 'mat movis', 2);
+insert into mat(id, materia, id_grado) values (4, 'mat capor', 2);
+insert into mat(id, materia, id_grado) values (5, 'mat count', 2);
 
 --prim
-insert into materia(id, materia, id_grado) values (6, 'escrit', 3);
-insert into materia(id, materia, id_grado) values (7, 'lect', 3);
-insert into materia(id, materia, id_grado) values (8, 'ed fis', 3);
+insert into mat(id, materia, id_grado) values (6, 'escrit', 3);
+insert into mat(id, materia, id_grado) values (7, 'lect', 3);
+insert into mat(id, materia, id_grado) values (8, 'ed fis', 3);
 
-insert into materia(id, materia, id_grado) values (9, 'escrit', 4);
-insert into materia(id, materia, id_grado) values (10, 'lect', 4);
-insert into materia(id, materia, id_grado) values (11, 'ed fis', 4);
+insert into mat(id, materia, id_grado) values (9, 'escrit', 4);
+insert into mat(id, materia, id_grado) values (10, 'lect', 4);
+insert into mat(id, materia, id_grado) values (11, 'ed fis', 4);
 
-insert into materia(id, materia, id_grado) values (12, 'escrit', 5);
-insert into materia(id, materia, id_grado) values (13, 'lect', 5);
-insert into materia(id, materia, id_grado) values (14, 'ed fis', 5);
+insert into mat(id, materia, id_grado) values (12, 'escrit', 5);
+insert into mat(id, materia, id_grado) values (13, 'lect', 5);
+insert into mat(id, materia, id_grado) values (14, 'ed fis', 5);
 
-insert into materia(id, materia, id_grado) values (16, 'escrit', 6);
-insert into materia(id, materia, id_grado) values (17, 'lect', 6);
-insert into materia(id, materia, id_grado) values (18, 'ed fis', 6);
+insert into mat(id, materia, id_grado) values (16, 'escrit', 6);
+insert into mat(id, materia, id_grado) values (17, 'lect', 6);
+insert into mat(id, materia, id_grado) values (18, 'ed fis', 6);
 
-insert into materia(id, materia, id_grado) values (19, 'escrit', 7);
-insert into materia(id, materia, id_grado) values (20, 'lect', 7);
-insert into materia(id, materia, id_grado) values (21, 'ed fis', 7);
-insert into materia(id, materia, id_grado) values (22, 'music', 7);
+insert into mat(id, materia, id_grado) values (19, 'escrit', 7);
+insert into mat(id, materia, id_grado) values (20, 'lect', 7);
+insert into mat(id, materia, id_grado) values (21, 'ed fis', 7);
+insert into mat(id, materia, id_grado) values (22, 'music', 7);
 
-insert into materia(id, materia, id_grado) values (23, 'escrit', 8);
-insert into materia(id, materia, id_grado) values (24, 'lect', 8);
-insert into materia(id, materia, id_grado) values (25, 'ed fis', 8);
-insert into materia(id, materia, id_grado) values (26, 'music', 8);
+insert into mat(id, materia, id_grado) values (23, 'escrit', 8);
+insert into mat(id, materia, id_grado) values (24, 'lect', 8);
+insert into mat(id, materia, id_grado) values (25, 'ed fis', 8);
+insert into mat(id, materia, id_grado) values (26, 'music', 8);
 
 
 --sec
-insert into materia(id, materia, id_grado) values (27, 'mat', 9);
-insert into materia(id, materia, id_grado) values (28, 'lite', 9);
-insert into materia(id, materia, id_grado) values (29, 'est soc', 9);
-insert into materia(id, materia, id_grado) values (30, 'music', 9);
+insert into mat(id, materia, id_grado) values (27, 'mat', 9);
+insert into mat(id, materia, id_grado) values (28, 'lite', 9);
+insert into mat(id, materia, id_grado) values (29, 'est soc', 9);
+insert into mat(id, materia, id_grado) values (30, 'music', 9);
 
-insert into materia(id, materia, id_grado) values (31, 'mat', 10);
-insert into materia(id, materia, id_grado) values (32, 'lite', 10);
-insert into materia(id, materia, id_grado) values (33, 'est soc', 10);
-insert into materia(id, materia, id_grado) values (34, 'music', 10);
+insert into mat(id, materia, id_grado) values (31, 'mat', 10);
+insert into mat(id, materia, id_grado) values (32, 'lite', 10);
+insert into mat(id, materia, id_grado) values (33, 'est soc', 10);
+insert into mat(id, materia, id_grado) values (34, 'music', 10);
 
-insert into materia(id, materia, id_grado) values (35, 'mat', 11);
-insert into materia(id, materia, id_grado) values (36, 'lite', 11);
-insert into materia(id, materia, id_grado) values (37, 'est soc', 11);
-insert into materia(id, materia, id_grado) values (38, 'music', 11);
+insert into mat(id, materia, id_grado) values (35, 'mat', 11);
+insert into mat(id, materia, id_grado) values (36, 'lite', 11);
+insert into mat(id, materia, id_grado) values (37, 'est soc', 11);
+insert into mat(id, materia, id_grado) values (38, 'music', 11);
 
-insert into materia(id, materia, id_grado) values (39, 'mat', 12);
-insert into materia(id, materia, id_grado) values (40, 'lite', 12);
-insert into materia(id, materia, id_grado) values (41, 'est soc', 12);
-insert into materia(id, materia, id_grado) values (42, 'music', 12);
+insert into mat(id, materia, id_grado) values (39, 'mat', 12);
+insert into mat(id, materia, id_grado) values (40, 'lite', 12);
+insert into mat(id, materia, id_grado) values (41, 'est soc', 12);
+insert into mat(id, materia, id_grado) values (42, 'music', 12);
 
-insert into materia(id, materia, id_grado) values (43, 'mat', 13);
-insert into materia(id, materia, id_grado) values (44, 'lite', 13);
-insert into materia(id, materia, id_grado) values (45, 'est soc', 13);
-insert into materia(id, materia, id_grado) values (46, 'music', 13);
-insert into materia(id, materia, id_grado) values (47, 'filos', 13);
+insert into mat(id, materia, id_grado) values (43, 'mat', 13);
+insert into mat(id, materia, id_grado) values (44, 'lite', 13);
+insert into mat(id, materia, id_grado) values (45, 'est soc', 13);
+insert into mat(id, materia, id_grado) values (46, 'music', 13);
+insert into mat(id, materia, id_grado) values (47, 'filos', 13);
 
-insert into materia(id, materia, id_grado) values (48, 'mat', 14);
-insert into materia(id, materia, id_grado) values (49, 'lite', 14);
-insert into materia(id, materia, id_grado) values (50, 'est soc', 14);
-insert into materia(id, materia, id_grado) values (51, 'music', 14);
-insert into materia(id, materia, id_grado) values (52, 'filos', 14);
+insert into mat(id, materia, id_grado) values (48, 'mat', 14);
+insert into mat(id, materia, id_grado) values (49, 'lite', 14);
+insert into mat(id, materia, id_grado) values (50, 'est soc', 14);
+insert into mat(id, materia, id_grado) values (51, 'music', 14);
+insert into mat(id, materia, id_grado) values (52, 'filos', 14);
 
 
 --
-
+/*
 ALTER TABLE ONLY public.nivel
    ADD CONSTRAINT nivel_id_plan_fkey FOREIGN KEY (id_plan) 
    REFERENCES public.plan(id) ON UPDATE CASCADE ON DELETE RESTRICT;
@@ -140,4 +144,4 @@ ALTER TABLE ONLY public.grado
 ALTER TABLE ONLY public.materia
    ADD CONSTRAINT materia_id_grado_fkey FOREIGN KEY (id_grado) 
    REFERENCES public.grado(id) ON UPDATE CASCADE ON DELETE RESTRICT;
-
+*/

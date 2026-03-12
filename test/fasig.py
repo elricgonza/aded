d4eda230142a2a47d4bdf3c245fee5ef350fdb46
elricgonza <elricgonza@gmail.com>
2026-03-12 15:23:16 -0400

    fasig.py

--- test/fasig.py
+++ test/fasig.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# -- dado curso(s) y grado(s) obtener materias de cada grado
+
+
 import psycopg2 as pg
 import psycopg2.extras as pge
 import psycopg2.extensions as pge
@@ -19,13 +22,21 @@ def get_cx():
         print(e)
         sys.exit()
 
+# -- fill asig
+def fill_asig():
+    cx1 = get_cx()
+    cur1 = cx1.cursor()
+    cx2 = get_cx()
+    cur2 = cx2.cursor()
+    cx3 = get_cx()
+    cur3 = cx3.cursor()
+
+    cur1.execute("SELECT * FROM curso where grado_id=1 ")
+    for r in cur1.fetchall():
+        print(r[1], r[2])
+
+    cx1.close()
+
 
 if __name__ == "__main__":
-    cx = get_cx()
-    cur = cx.cursor()
-    cur.execute("SELECT * FROM mat where grado_id = 1")
-    for r in cur.fetchall():
-        print(r)
-    cur.close()
-    cx.close()
-
+    fill_asig()

d0cc2ba7d5b610abdb4ff0484018a4750d250eb4
elricgonza <elricgonza@gmail.com>
2026-05-12 17:28:21 -0400

    f_mat

---
+++ test/f_materia.sql
@@ -0,0 +1,62 @@
+--inic
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (1, 'juegos', 1, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (2, 'bailes', 1, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (3, 'juegos', 2, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (4, 'ed fis', 2, now(), now(), 1);
+--prim
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (5, 'matem', 3, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (6, 'leng', 3, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (7, 'ed fis', 3, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (8, 'matem', 4, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (9, 'leng', 4, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (10 'ed fis', 4, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (11, 'matem', 5, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (12, 'leng', 5, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (13, 'ed fis', 5, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (14, 'matem', 6, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (15, 'leng', 6, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (16, 'ed fis', 6, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (17, 'relig', 6, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (18, 'matem', 7, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (19, 'leng', 7, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (20, 'ed fis', 7, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (21, 'relig', 7, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (22, 'matem', 8, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (23, 'leng', 8, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (24, 'ed fis', 8, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (25, 'relig', 8, now(), now(), 1);
+
+--sec
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (26, 'matem', 9, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (27, 'leng', 9, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (28, 'ed fis', 9, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (29, 'matem', 10, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (30, 'leng', 10, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (31, 'ed fis', 10, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (32, 'matem', 11, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (33, 'leng', 11, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (34, 'ed fis', 11, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (35, 'matem', 12, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (36, 'leng', 12, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (37, 'ed fis', 12, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (38, 'biolog', 12, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (39, 'matem', 13, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (40, 'leng', 13, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (41, 'ed fis', 13, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (42, 'biolog', 13, now(), now(), 1);
+
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (43, 'matem', 14, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (44, 'leng', 14, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (45, 'ed fis', 14, now(), now(), 1);
+insert into materia(id, materia, gra_id, creado, act, usu_id) values (46, 'biolog', 14, now(), now(), 1);

631b3f68c81f7d283881d70bbfa7fdb3bb804a22
elricgonza <elricgonza@gmail.com>
2026-05-19 12:13:30 -0400

    test new tok

--- test/f_nota.py
+++ test/f_nota.py
@@ -13,30 +13,6 @@ DB_CONFIG = {
     "password": "paded",  # <-- cambia aquí
 }
 
-# ── Parámetros del negocio ─────────────────────────────────────────────────────
-MAX_ALUMNOS_POR_CURSO = 40   # límite de alumnos por curso
-FECHA_BASE            = date(2026, 1, 15)   # fecha mínima de inscripción
-FECHA_TOP             = date(2026, 12, 30)  # fecha máxima de inscripción
-
-def fecha_aleatoria(ini: date, fin: date) -> date:
-    delta = (fin - ini).days
-    return ini + timedelta(days=random.randint(0, delta))
-
-def get_cuotas_por_curso(cur_id):
-    cnx = psycopg2.connect(**DB_CONFIG)
-    cur = cnx.cursor()
-    try:
-        cur.execute(f"select nro_cuota, cuota from costo where cur_id = {cur_id}")
-        cuotas = cur.fetchall()
-    except psycopg2.Error as e:
-        print(f"Error al obtener cuotas para curso {cur_id}: {e}")
-        cuotas = []
-    finally:
-        cur.close()
-        cnx.close()
-    return cuotas
-
-
 def poblar_notas():
     conn = psycopg2.connect(**DB_CONFIG)
     cur  = conn.cursor()
@@ -47,55 +23,58 @@ def poblar_notas():
         cur.execute("ALTER SEQUENCE nota_id_seq RESTART WITH 1;")  # reiniciar secuencia de IDs
 
         # ── 1. Leer IDs disponibles ────────────────────────────────────────────
-        cur.execute("SELECT a.id, a.cur_id FROM inscrito a, curso b where a.cur_id=b.id and b.gestion=2026 ORDER BY a.id;")
+        sqltxt = '''
+            select  ins.id as ins_id,
+                mat.id as mat_id 
+            from inscrito ins, curso cur, 
+                asignado asi, materia mat
+            where
+                ins.cur_id = cur.id
+                and cur.id = asi.cur_id
+                and mat.id = asi.mat_id
+            order by ins.id, mat.id
+        '''
+        cur.execute(sqltxt)
         inscritos = [(r[0], r[1]) for r in cur.fetchall()]          # 
         print(inscritos[:5])  # mostrar primeros 5 para ver formato
-        print(inscritos[0][0])
-        print(inscritos[0][1])
-        print(inscritos[2][0])
-        print(inscritos[2][1])
-
 
         # ── 1. Construir pagos 
-        #    por curso el nro de registros en costo
-        pagos = []
-        pagado = False
-        metodo_pago = ""
-        fecha_pago =  '2000-01-01'
-        referencia_pago = '2000-01-01'
+        #    misma cant q ins 
+        notas = []
+        nota1, nota2, nota3, nota_final  = 0, 0, 0, 0
+        nota_aprob = 51
+        aprobado = False
         obs = ""
         creado        = date.today()
         act           = date.today()
         usu_id        = 1  # usuario ficticio
 
-        pag_id        = 1  # usuario ficticio   
+        not_id        = 1
 
         for inscrito in inscritos:
-            ins_id, cur_id = inscrito
-            cuotas = get_cuotas_por_curso(cur_id)
-            for cuota in cuotas:
-                nro_cuota, cuota = cuota
+            ins_id, mat_id = inscrito
 
-                pagos.append((pag_id, ins_id, nro_cuota, cuota, pagado, metodo_pago, \
-                                  fecha_pago, referencia_pago, obs, creado, act, usu_id))
-                pag_id += 1
+            notas.append((not_id, ins_id, mat_id, nota1, nota2, nota3, nota_final, \
+                              nota_aprob, aprobado, obs, creado, act, usu_id))
+            not_id += 1
 
-                # ── 4. Insertar en lotes ───────────────────────────────────────────────
-                insert_sql = """
-                    INSERT INTO pago (id, ins_id, nro_cuota, cuota, pagado, metodo_pago, \
-                        fecha_pago, referencia_pago, obs, creado, act, usu_id)
-                    VALUES (%s, %s, %s, %s,  %s, %s, %s, %s,  %s, %s, %s, %s)
-                    ON CONFLICT DO NOTHING;
-                """
-        cur.executemany(insert_sql, pagos)
+            # ── 4. Insertar en lotes ───────────────────────────────────────────────
+            insert_sql = """
+                INSERT INTO nota (id, ins_id, mat_id, nota1, nota2, nota3, nota_final, \
+                    nota_aprob, aprobado, obs, creado, act, usu_id)
+                VALUES (%s, %s, %s, %s,  %s, %s, %s, %s,  %s, %s, %s, %s, %s)
+                ON CONFLICT DO NOTHING;
+            """
+
+        cur.executemany(insert_sql, notas)
         conn.commit()
 
         # ── 5. Reporte ─────────────────────────────────────────────────────────
-        cur.execute("SELECT COUNT(*) FROM pago;")
+        cur.execute("SELECT COUNT(*) FROM nota;")
         total = cur.fetchone()[0]
 
-        print(f"✔  Pagos insertados         : {len(pagos)}")
-        print(f"✔  Total en tabla pago      : {total}")
+        print(f"✔  Inscritos : {len(inscritos)}")
+        print(f"✔  Total en tabla nota      : {total}")
         print(f"   Alumnos inscritos        : {len(inscritos)}")
     except psycopg2.Error as e:
        conn.rollback()
@@ -106,4 +85,4 @@ def poblar_notas():
         conn.close()
 
 if __name__ == "__main__":
-    poblar_pagos()
+    poblar_notas()

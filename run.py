0973ae81aa1c0e5fa36cc1a88248ca9591526192
elricgonza <elricgonza@gmail.com>
2026-05-22 16:46:41 -0400

    correccion filtros, pag

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Role': Role, 'Permission': Permission}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=6001)
